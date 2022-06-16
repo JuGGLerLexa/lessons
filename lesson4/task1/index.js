85a5b1f27cc49aec3adfbf2533fbb74281d8daf2
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-16 19:47:35 +0300

    in progress

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -4,7 +4,11 @@
 // импортируйте ф-ции sum и mult под такими же именами, а так же ф-цию calc под именем calcExpression из файла calculator/index.js
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
-import calcResult from './calculator/index.js';
+import calcResult, { sum, mult } from './calculator/index.js';
+import fetchUser from './profile/gateway.js';
+import { printProfile } from './profile/index.js';
+import './polyfills/array-flat.js';
+import './polyfills/array-flat.js';
 
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);

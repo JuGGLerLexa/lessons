0399cb49b04b778a39d72b9fdc09603a4a4bbce2
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-17 15:01:13 +0300

    fix

--- lesson4/task1/polyfills/array-flat.js
+++ lesson4/task1/polyfills/array-flat.js
@@ -1,11 +1,11 @@
-// file path: /polyfills/array-flatMap.js
+// file path: /polyfills/array-flat.js
 
 // здесь никакие экспорты не нужны
 
-console.log('polyfill for flatMap');
+console.log('polyfill for flat');
 
-window.Array.prototype.flatMap =
-    Array.prototype.flatMap ||
-    function flatMap() {
+Array.prototype.flat =
+    Array.prototype.flat ||
+    function flat() {
         // ... implementation for older browsers
     };

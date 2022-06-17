b3762d3945afa72945f7b991925c60a900c8a65c
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-17 14:42:14 +0300

    fix

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -8,7 +8,7 @@ import calcExpression, { sum, mult } from './calculator/index.js';
 import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 import './polyfills/array-flat.js';
-import './polyfills/array-flat.js';
+import './polyfills/array-flatMap';
 
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);

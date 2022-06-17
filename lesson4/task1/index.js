6e16e961cb28b8f9ccda7536be5023eedda4c985
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-17 14:42:48 +0300

    fiox

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -8,7 +8,7 @@ import calcExpression, { sum, mult } from './calculator/index.js';
 import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 import './polyfills/array-flat.js';
-import './polyfills/array-flatMap';
+import './polyfills/array-flatMap.js';
 
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);

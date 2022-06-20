ec70da14d419d9cc749b0bfa66a3eafdae392b2e
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-20 21:52:10 +0300

    webpack

--- lesson5/task2/webpack.config.js
+++ lesson5/task2/webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: { 
+    entry: {
         profile: './src/profile/index.js',
-        dashboard: './src/dashboard/index.js'
+        dashboard: './src/dashboard/index.js',
     },
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, build),
+        path: path.join(__dirname, 'build'),
     },
-    watch: true,
 };

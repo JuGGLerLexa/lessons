5d706f8f2806d0ed318afcfff4b4b36ad808af25
Олексій <71986453+JuGGLerLexa@users.noreply.github.com>
2022-06-22 15:53:34 +0300

    Todo List + Webpack - DevServer

--- lesson8/task2/webpack.config.js
+++ lesson8/task2/webpack.config.js
@@ -1,61 +1,62 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'prodaction'
+    const isProduction = argv.mode === "production";
     const config = {
-        entry: './src/index.js',
+        entry: "./src/index.js",
         output: {
-            filename: 'bundle.js',
+            filename: "bundle.js",
         },
         module: {
             rules: [
                 {
-                    test: /.js$/i,
-                    use: ['babel-loader'],
-                },
-                {
                     test: /.s?css$/,
                     use: [
-                        isProduction
-                            ? MiniCssExtractPlugin.loader
-                            : 'style-loader',
-                        'css-loader',
-                        'sass-loader',
+                        isProducton ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader",
+                        "sass-loader",
                     ],
                 },
                 {
-                    test: /.(jpg|png)$/i,
+                    test: /.js$/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /.(jpg|png)$/,
                     use: [
                         {
-                            loader: 'url-loader',
+                            loader: "url-loader",
                             options: {
                                 limit: 8192,
-                                name: '[name].[ext]',
-                                outputPath: 'images',
+                                name: "[name].ext",
+                                outputPath: "images",
                             },
+
                         },
                     ],
                 },
             ],
         },
         plugins: [
-            new webpack.ProgressPlugin(),
             new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: './src/index.html'
             }),
         ],
         devServer: {
-            hot: true
-        }
+            port: 9000,
+            hot: true,
+        },
     };
+
     if (isProduction) {
-        config.plugins.push(new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }));
+        config.plugins.push(
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            })
+        );
     }
     return config;
 };
